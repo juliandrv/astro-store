01977d60ab29be333b712f09ac1ceee61a648663
Julián Ramírez <judramirezva@gmail.com>
2024-09-04 23:06:58 -0500

    Started Astro Store project

---
+++ db/seed.ts
@@ -0,0 +1,30 @@
+import { db, Role, User } from 'astro:db';
+import { v4 as UUID } from 'uuid';
+import bcrypt from 'bcryptjs';
+
+// https://astro.build/db/seed
+export default async function seed() {
+  const roles = [
+    { id: 'admin', name: 'Administrator' },
+    { id: 'user', name: 'User' },
+  ];
+
+  const diana = {
+    id: UUID(),
+    name: 'Diana',
+    email: 'diana@example.com',
+    password: bcrypt.hashSync('123456'),
+    role: 'admin',
+  };
+
+  const naty = {
+    id: UUID(),
+    name: 'Naty',
+    email: 'naty@example.com',
+    password: bcrypt.hashSync('123456'),
+    role: 'user',
+  };
+
+  await db.insert(Role).values(roles);
+  await db.insert(User).values([diana, naty]);
+}
