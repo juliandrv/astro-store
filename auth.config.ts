323156e9fe3c608c8abc779074627245ef153d9d
Julián Ramírez <judramirezva@gmail.com>
2024-09-05 22:46:23 -0500

    initial config authjs

---
+++ auth.config.ts
@@ -0,0 +1,47 @@
+// import GitHub from '@auth/core/providers/github';
+import { defineConfig } from 'auth-astro';
+import Credentials from '@auth/core/providers/credentials';
+import { db, eq, User } from 'astro:db';
+import bcrypt from 'bcryptjs';
+
+export default defineConfig({
+  providers: [
+    // TODO:
+    // GitHub({
+    //   clientId: import.meta.env.GITHUB_CLIENT_ID,
+    //   clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+    // }),
+
+    Credentials({
+      credentials: {
+        email: {
+          label: 'Correo',
+          type: 'email',
+        },
+        password: {
+          label: 'Contraseña',
+          type: 'password',
+        },
+      },
+      authorize: async ({ email, password }) => {
+        const [user] = await db
+          .select()
+          .from(User)
+          .where(eq(User.email, `${email}`));
+
+        if (!user) {
+          throw new Error('User not found');
+        }
+
+        if (!bcrypt.compareSync(password as string, user.password)) {
+          throw new Error('Invalid Password');
+        }
+
+        const { password: _, ...rest } = user;
+        console.log(rest);
+
+        return rest;
+      },
+    }),
+  ],
+});
