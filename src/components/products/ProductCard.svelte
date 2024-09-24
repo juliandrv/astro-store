c5fa4b38232cdbd16532ecd047d8e71f73508cca
Julián Ramírez <judramirezva@gmail.com>
2024-09-23 23:12:58 -0500

    Added ProductCard content | Added Pagination

--- src/components/products/ProductCard.svelte
+++ src/components/products/ProductCard.svelte
@@ -1,7 +1,40 @@
 <script lang="ts">
-  import type { ProductWithImages } from "@/interfaces";
+  import { fade } from 'svelte/transition';
 
-  export let product: ProductWithImages
+  import type { ProductWithImages } from '@/interfaces';
+
+  export let product: ProductWithImages;
+
+  const images = product.images.split(',').map((img) => {
+    return img.startsWith('http')
+      ? img
+      : `${import.meta.env.PUBLIC_URL}/images/products/${img}`;
+  });
+
+  let hovered = false;
+  let currenImage = images[0];
 </script>
 
-<div>{product.title}</div>
+<a href={`product/${product.slug}`}>
+  <img
+    on:mouseenter={() => (
+      (currenImage = images[1] ?? images[0]), (hovered = true)
+    )}
+    on:mouseleave={() => (
+      (currenImage = images[0]), (hovered = false)
+    )}
+    src={currenImage}
+    alt={product.title}
+    class="h-[350px] object-contain transition-all"
+    transition:fade={{ duration: 300 }}
+  />
+  <h4>{product.title}</h4>
+  <p>${product.price}</p>
+</a>
+
+<style>
+  img {
+    transition: opacity 0.3s ease;
+    opacity: 1;
+  }
+</style>
--- src/components/products/ProductList.svelte
+++ src/components/products/ProductList.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-  import type { ProductWithImages } from '@/interfaces'
+  import type { ProductWithImages } from '@/interfaces';
   import ProductCard from './ProductCard.svelte';
 
-  export let products: ProductWithImages[]
+  export let products: ProductWithImages[];
 </script>
 
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center">
+<div
+  class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center"
+>
   {#each products as product}
     <ProductCard {product} />
   {/each}
