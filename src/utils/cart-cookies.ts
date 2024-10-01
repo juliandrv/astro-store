3e8ada14d93eab76c2b86108ca16e5f5891ac8db
Julián Ramírez <judramirezva@gmail.com>
2024-09-30 22:59:43 -0500

    Added Cookies File

---
+++ src/utils/cart-cookies.ts
@@ -0,0 +1,38 @@
+import type { CartItem } from '@/interfaces';
+import Cookies from 'js-cookie';
+
+export class CartCookiesCLient {
+  static getCart(): CartItem[] {
+    return JSON.parse(Cookies.get('cart') ?? '[]');
+  }
+
+  static addItem(cartItem: CartItem): CartItem[] {
+    const cart = CartCookiesCLient.getCart();
+    const itemInCart = cart.find(
+      (item) =>
+        item.productId === cartItem.productId &&
+        item.size === cartItem.size
+    );
+
+    if (itemInCart) {
+      itemInCart.quantity += cartItem.quantity;
+    } else {
+      cart.push(cartItem);
+    }
+
+    Cookies.set('cart', JSON.stringify(cart));
+
+    return cart;
+  }
+
+  static removeItem(productId: string, size: string): CartItem[] {
+    const cart = CartCookiesCLient.getCart();
+    const updatedCart = cart.filter(
+      (item) => !(item.productId === productId && item.size === size)
+    );
+
+    Cookies.set('cart', JSON.stringify(updatedCart));
+
+    return updatedCart;
+  }
+}
